99dad3e5b47832dc0f5e5091ba75427b610763af
Fajri Rinaldi Chan <fajri@gariskode.com>
2025-01-25 11:40:42 +0700

    feat: add Websocket for create session

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { config } = require("dotenv");
+const { toDataURL } = require("qrcode");
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
@@ -8,6 +9,7 @@ const path = require("path");
 const MainRouter = require("./app/routers");
 const errorHandlerMiddleware = require("./app/middlewares/error_middleware");
 const whatsapp = require("wa-multi-session");
+const WebSocket = require('ws');
 
 config();
 
@@ -46,3 +48,86 @@ whatsapp.onConnecting((session) => {
 });
 
 whatsapp.loadSessionsFromStorage();
+
+
+
+const WS_PORT = process.env.WS_PORT || "5003";
+const wsServer = http.createServer();
+const wss = new WebSocket.Server({ server: wsServer });
+wsServer.listen(WS_PORT, () => {
+  console.log('WebSocket server running on ws://localhost:' + WS_PORT);
+});
+
+wss.on('connection', (ws) => {
+  console.log('New client connected');
+
+  ws.on('message', (data) => {
+    try {
+      const jsonData = JSON.parse(data);
+      console.log('Message received: ', jsonData);
+
+      const sessionName = jsonData.session;
+      if (!sessionName) {
+        ws.send(
+          JSON.stringify({
+            type: 'error',
+            message: 'Session name is required',
+          })
+        );
+        return;
+      } else {
+        whatsapp.onQRUpdated(async (data) => {
+          const qr = await toDataURL(data.qr);
+          if (data.sessionId == sessionName) {
+            ws.send(
+              JSON.stringify({
+                type: 'response',
+                qr: qr,
+              })
+            );
+          } else {
+            ws.send(
+              JSON.stringify({
+                type: 'response',
+                qr: qr,
+              })
+            );
+          }
+        });
+
+      }
+      whatsapp.startSession(sessionName, { printQR: true }).then(() => {
+        ws.send(
+          JSON.stringify({
+            type: 'response',
+            message: 'Session started',
+          })
+        );
+      }
+      ).catch((error) => {
+        // console.error('Error starting session: ', error);
+        console.error('Error starting session');
+        ws.send(
+          JSON.stringify({
+            type: 'error',
+            message: 'Kamu sudah Terhubung, Jika ada masalah silahkan reset session',
+          })
+        );
+      });
+
+    } catch (error) {
+      console.error('Error parsing JSON message: ', error);
+      ws.send(
+        JSON.stringify({
+          type: 'error',
+          message: 'Error parsing JSON message',
+        })
+      );
+    }
+
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
